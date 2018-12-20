aeae515a67d1cfadc37746e98f3ce5548eef3149
Anna Stein <ann.m.stein@gmail.com>
2018-12-19 21:55:02 -0500

    my work

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [{
+    name: "Bob",
+    photo: "https://i4w8j7y6.stackpathcdn.com/wp-content/uploads/2016/02/Animals.jpg",
+    scores: [4, 2, 3, 1, 5, 5, 4, 3, 1, 2]
+},
+{
+    name: "Joe",
+    photo: "https://www.askideas.com/media/12/Funny-Baby-Pig-With-High-Neck-Shoes.jpg",
+    scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
+}, 
+{
+    name: "Kate",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaMWSiB4WTBUHo5gIHVFltoscoOhQ__TqWlRNgxYigquDh8aE7Ug",
+    scores: [2, 3, 4, 5, 4, 3, 2, 1, 2, 3]
+},
+{
+    name: "Frank",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6A0KedyeK_R37KcZbAGAH6kbmxj0ZR_U9_2sYeP9BkBdGDTRM",
+    scores: [3, 4, 5, 4, 3, 3, 2, 2, 1, 5]
+},
+{
+    name: "Suzi",
+    photo: "https://www.thoughtco.com/thmb/Lx64PgK7CyW7SNdFZW2zYXtfWG4=/640x512/filters:no_upscale()/husky-funny-face-amazing-creture-blogspot-58b8ed403df78c353c30d321.jpg",
+    scores: [1, 3, 5, 4, 1, 2, 3, 3, 4, 3]
+}];
+
+module.exports = friends;
